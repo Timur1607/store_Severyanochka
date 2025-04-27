e8da2b789fd99d3593efa610d585332dd9996551
Timur <abkeevtimur@gmail.com>
2025-04-27 12:50:37 +0300

    first commit

---
+++ src/Components/Container/Container.jsx
@@ -0,0 +1,10 @@
+import s from './Container.module.css'
+export default function Container(props){
+    return(
+        <>
+            <div className={s.container}>
+                {props.children}
+            </div>
+        </>
+    )
+}
