c2c08537ca80a4d811722249387003c4bf6e2fa7
Vladimir <adda37@yandex.ru>
2025-05-18 11:16:25 +0300

    add special+map sections

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -2,13 +2,15 @@ import Promotion from '../Promotion/Promotion'
 import NewProducts from '../NewProducts/NewProducts'
 import s from './Main.module.css'
 import Special from '../Special/Special'
+import Map from '../Map/Map'
 export default function Main() {
     return (
         <>
             <h1 className={s.h1}>Интернет-магазин Северяночка</h1>
             <Promotion />
             <NewProducts />
-            {/* <Special/> */}
+            <Special/>
+            <Map/>
         </>
     )
 }
